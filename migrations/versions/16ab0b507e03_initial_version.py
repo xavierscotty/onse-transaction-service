f1c3025de77a7dc644bacd1f0d0fa139857573c2
Tom Oram <tom.oram@armakuni.com>
2019-01-09 13:55:13 +0000

    [#162982150] Add alembic migrations

---
+++ migrations/versions/16ab0b507e03_initial_version.py
@@ -0,0 +1,28 @@
+"""initial version
+
+Revision ID: 16ab0b507e03
+Revises: 
+Create Date: 2019-01-09 10:44:18.137854
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '16ab0b507e03'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'transactions',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('account_number', sa.String()),
+        sa.Column('amount', sa.Integer())
+    )
+
+
+def downgrade():
+    op.drop_table('transactions')
